2ef6e1e5180c21da1ecd7249b462bb04d1439f66
Simon Strandgaard <neoneye@gmail.com>
2014-06-12 18:26:43 +0200

    Added tests

--- Tests/LambaSpecificationTests.swift
+++ Tests/LambaSpecificationTests.swift
@@ -11,15 +11,38 @@ class LambdaSpecificationTests: XCTestCase {
 		XCTAssertFalse(spec.isSatisfiedBy("hell"))
 		XCTAssertFalse(spec.isSatisfiedBy(""))
 		XCTAssertFalse(spec.isSatisfiedBy("nope"))
+		XCTAssertFalse(spec.isSatisfiedBy(1234))
+		XCTAssertFalse(spec.isSatisfiedBy(nil))
 	}
 	
-	func testInt0() {
+	func testInteger0() {
 		let spec = LambdaSpecification.lambda({ (candidate: Int) -> Bool in
 			return candidate > 5;
 			})
 		XCTAssertTrue(spec.isSatisfiedBy(10))
-		XCTAssertTrue(spec.isSatisfiedBy(10))
 		XCTAssertFalse(spec.isSatisfiedBy(0))
+		XCTAssertFalse(spec.isSatisfiedBy("not integer"))
+		XCTAssertFalse(spec.isSatisfiedBy(nil))
+		XCTAssertFalse(spec.isSatisfiedBy(10.0)) // float is not integer, thus it's false
+	}
+	
+	func testCustomer0() {
+		class Customer {
+			let name: String
+			init(name: String) {
+				self.name = name
+			}
+		}
+		
+		let spec = LambdaSpecification.lambda({ (candidate: Customer) -> Bool in
+			return candidate.name.hasPrefix("john");
+			})
+		XCTAssertTrue(spec.isSatisfiedBy(Customer(name: "john doe")))
+		XCTAssertFalse(spec.isSatisfiedBy(Customer(name: "jane doe")))
+		XCTAssertFalse(spec.isSatisfiedBy(0))
+		XCTAssertFalse(spec.isSatisfiedBy("not a customer"))
+		XCTAssertFalse(spec.isSatisfiedBy(nil))
+		XCTAssertFalse(spec.isSatisfiedBy(10.0))
 	}
 	
 }
